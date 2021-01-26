8c59865b537a7abf2d536d7e0cf2013ccd6062bb
Россия <ramzans095@mail.ru>
2021-01-26 09:41:39 +0300

    Написал функцию для вывода символа в указанной точке.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,15 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            Draw(2, 5, '*');
+            Console.ReadLine();
+        }
+        static void Draw (int x, int y, char s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
         }
     }
 }
